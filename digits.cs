cbd034b10bc2c2a5e5d9ae97499629e190b2d1c8
Carolina Parraga <carol_parraga@hotmail.com>
2020-11-11 10:51:38 +0100

    The file has been cleaned

--- digits.cs
+++ digits.cs
@@ -1,35 +1,37 @@
 using System;
 
+//digit counter of a number entered by the user
+using System;
+
 class digits
 {
- static void Main()
- {
-	 //counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
-     int a = 0, b = 0, c = 0, d = 0;
-      Console.Write("Insert a number: ");
-      // read the number as string to check the word "end"
-      string e = Console.ReadLine();
-     while(e!="end"){
-int input = Convert.ToInt32(e);
-       if(input != 0)
-{
-       if(input/10 == 0)
-           a ++;  // it only has one digit
-       else 
-       if (input/100 == 0)
-		b ++;  // the number has 2 digits
-        else
-        if (input/1000 == 0)
-        c ++;  // the number has 3 digits
-        else
-           d++;  // the number has more than 3 digits
-        }
-      e = Console.ReadLine();
+    static void Main()
+    {
+	
+	int number;
+		  
+	string dato = " ";
+	
+		
+	Console.Write("Insert a number: ");
+	dato = Console.ReadLine();
+		number = Convert.ToInt32(dato);
+        
+        //repeat proces until number 0
+	while (number != 0)
+	{
+	    if(number/10 == 0)
+	    Console.WriteLine("The number has one digit");
+	        else if (number/100 == 0)
+		Console.WriteLine("The number has two digit");
+		    else if (number/1000 == 0)
+		    Console.WriteLine("The number has three digit");
+		        else if (number/10000 ==0)
+			Console.WriteLine("The number has four digit");
+								    
+			Console.WriteLine("Introduce un numero:");
+			number = Convert.ToInt32(Console.ReadLine());
+		}
+					    
+	}
 }
-
-Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d); 
-
-
- }
-}
-
