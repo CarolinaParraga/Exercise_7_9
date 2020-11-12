4aedada1883425bdc12c7aa9ceb7a2278e3731f7
Carolina Parraga <carol_parraga@hotmail.com>
2020-11-12 09:50:12 +0100

    The file has been cleaned

--- average.cs
+++ average.cs
@@ -1,20 +1,28 @@
 using System;
-public class Average{
-public static void Main(){
-double a=0, b, c=0, d;
-string s;
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
-while(s!="end"){  // if the mark isn't an end then we pass it to double to work with it
-b=Convert.ToDouble(s);
-c++;   //counter of numbers
-a+=b;  // sum of all the notes
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();
-}
-if(c!=0){
-	d=a/c;  //calculate the average
-Console.WriteLine("The average is {0}.",d);
-}
-}
+public class Average
+{
+    public static void Main()
+    {
+	double note = 0.0;
+	double average = 0.0;
+	double sum = 0.0;
+	int counter = 1; 
+	string mark = " ";
+		
+	Console.Write("Insert a note: ");
+	mark = Console.ReadLine();
+	note = Convert.ToDouble(mark);	
+			
+	//repeat proces until enter "end" or note insn't 0
+        while(mark!="end" && note !=0)
+        {
+	    Console.Write("Insert a note: ");
+	    mark = Console.ReadLine();
+	    note = Convert.ToDouble(mark);
+	    sum = sum+note;  
+	    counter++;
+	    average=sum/counter;  //calculate the average
+	    Console.WriteLine("The average is {0}.",average);
+        }
+     }
 }
